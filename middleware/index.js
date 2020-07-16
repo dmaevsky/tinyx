9ce1b1681cef4a35661ce5cf64e0d40741850b2f
Dmitry Maevsky <dmitry.maevsky@gmail.com>
2020-07-16 23:15:35 +0900

    applyMiddleware fix

--- middleware/index.js
+++ middleware/index.js
@@ -1,3 +1,3 @@
-export default applyMiddleware = (store, middleware = []) => {
+export default function applyMiddleware(store, middleware = []) {
   return middleware.reduceRight((acc, mw) => mw(acc), store);
 }
