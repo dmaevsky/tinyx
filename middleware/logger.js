964b3e900037339088413c180e5b8217c501c0c5
Dmitry Maevsky <dmitry.maevsky@gmail.com>
2021-03-07 15:14:43 +0900

    customize print in logger

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,15 +1,15 @@
-const txLogger = ({ commit, ...rest }) => ({
+export const logger = (print = console.log) => ({ commit, ...rest }) => ({
   commit: (transaction, payload, ...keyPath) => {
     const changes = commit(transaction, payload, ...keyPath);
 
     let msg = keyPath.length ? keyPath.join('.') + ' [' : '[';
     msg += transaction.name + ']';
-    if (transaction.length) console.log(msg + ':', payload, changes);
-    else console.log(msg, changes);
+    if (transaction.length) print(msg + ':', payload, changes);
+    else print(msg, changes);
 
     return changes;
   },
   ...rest
 });
 
-export default txLogger;
+export default logger();
